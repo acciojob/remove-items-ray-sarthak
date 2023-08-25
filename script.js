a6dc96ecbd907a302ef7d27e6dcdd27be8f5bd65
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-08-25 22:49:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const color=document.getElementById("colorSelect");
+const button=document.getElementsByTagName("input")[0];
+
+const arr=color.children;
+button.addEventListener("click",deleteit);
+ function deleteit(){
+	 let inputed=color.value;
+	console.log(inputed);
+	 for(let i=0;i<arr.length;i++){
+		 if(arr[i].value===inputed){
+			 color.removeChild(arr[i]);
+		 }
+	 }
+}
